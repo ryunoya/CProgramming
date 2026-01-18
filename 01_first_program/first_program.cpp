c3f25934014c7001c6e8710cfcd3a91aab4a8578
ghowell9695 <ghowell9695@floridapoly.edu>
2026-01-18 19:30:32 +0000

    new file:   01_first_program/a.out 	new file:   01_first_program/first_program.cpp

---
+++ 01_first_program/first_program.cpp
@@ -0,0 +1,21 @@
+/*
+    The first C++ program
+    01.14.26
+*/
+
+#include <iostream>
+
+// Create the main function
+int main(void) {   
+    std::cout << "Hello from COP 3337 !\n" << std::endl;
+
+    std::cout << "(15 * 78) + 14 is " << (15 * 78) + 14 << std::endl;
+
+    //std - standard namespace
+    //cout - console output (object of iostream ["in out stream"] class)
+    // << - stream insertion operator
+    //endl - end line (move the cursor in the terminal in the next line)
+
+
+    return 0;
+}
