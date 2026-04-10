36b88c3a5a75199e63fcb6341b7025844c364d18
ghowell9695 <ghowell9695@floridapoly.edu>
2026-04-10 13:20:15 +0000

    new file:   23_operator_overloading/Point.cpp 	new file:   23_operator_overloading/Point.hpp 	modified:   23_operator_overloading/mainPoint.cpp

--- 23_operator_overloading/mainPoint.cpp
+++ 23_operator_overloading/mainPoint.cpp
@@ -2,46 +2,24 @@
 #include <iostream>
 
 int main(void) {
-    // create a point
-    Point p1(3,4);
-    Point p2(5,4);
-    Point p3(50,70);
+    Point p1(3, 4, "my point");
 
-//     // i want to compare 2 points
-//     if (p1 != p2) {
-//         std::cout << (char) p3[0];
+    Point p2(p1);
+    Point p3;
 
-//     } else {
-//         std::cout << (char) p3[1];
-//     }
+    Point p4(1, 1, "p4");
 
-//     p1[0] = 52;
-//     std::cout << (char) p1[0];
-//     std::cout << "Y5B";
+    p3 = p1 = p4;
 
-    Point p4 = p1 + p2;
-
-    std::cout <<  p4.toString() << std::endl;
-
-    p4 += p3;
-    std::cout <<  p4.toString() << std::endl;
-
-    int x += 1;
-    (x += 2) += 5;
-    std:: cout << x << std::endl;
-
-    Point p5 = -p4;
-    std::cout <<  p5.toString() << std::endl;
-
-    // test preincrement
-    ++p5;
-    std::cout <<  p5.toString() << std::endl;
-
-
-
-
+    std::cout << p1.toString() << std::endl;
+    std::cout << p2.toString() << std::endl;
+    std::cout << p3.toString() << std::endl;
+    std::cout << p4.toString() << std::endl;
     
+    std::cout << "---" << p1 << std::endl;
+
+    std::cin >> p4;
+    std::cout << p4;
+
     return 0;
 }
-
-
